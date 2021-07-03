742bd2bc32503799a730a81e28d0c16d3ed6cf9a
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-07-03 09:20:12 +0530

    Update userinfo.py

--- YoneRobot/modules/userinfo.py
+++ YoneRobot/modules/userinfo.py
@@ -241,7 +241,7 @@ def info(update: Update, context: CallbackContext):
     rep = message.reply_text("<code>❂CONNECTING TO DXD SERVER❂</code>", parse_mode=ParseMode.HTML)
 
     text = (
-        f"╒══「✪<b> Appraisal results✪</b> 」\n"
+        f"╒═══「✪<b> Appraisal results</b> 」\n"
         f"✯ID: <code>{user.id}</code>\n"
         f"✯First Name: {html.escape(user.first_name)}"
     )
