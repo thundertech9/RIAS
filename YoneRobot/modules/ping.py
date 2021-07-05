e5e592264645909d9de5eb024957986b3c554591
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-07-05 14:27:48 +0530

    Update ping.py

--- YoneRobot/modules/ping.py
+++ YoneRobot/modules/ping.py
@@ -80,9 +80,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "BOOST! BOOST!\n"
+        "BOOST!!\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>RIAS[.](https://telegra.ph/file/3db16b9836175836b1058.jpg)uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>Rias uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
