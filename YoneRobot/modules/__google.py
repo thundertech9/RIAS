b76f14af48d95c9e6cd918e94a0bab07c47930da
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-07-05 21:20:05 +0530

    Update __google.py

--- YoneRobot/modules/__google.py
+++ YoneRobot/modules/__google.py
@@ -287,5 +287,5 @@ __help__ = """
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
  ❍ Yone <query>*:* Yone answers the query
-  💡Ex: `Yone where is India?`
+  💡Ex: `Rias where is India?`
 """
