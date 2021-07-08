ade8f7e1e942e0d699244281d5534a4347aa4e90
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-07-08 08:39:14 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -358,7 +358,7 @@ def yone_about_callback(update, context):
                  \n❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  \n❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  \n❍ I check for admins' permissions before executing any command and more stuffs
-                 \n\n_yone's licensed under the GNU General Public License v3.0_
+                 \n\n_Rias's licensed under the GNU General Public License v3.0_
                  \nHere is the [💾Repository](https://github.com/AnimeKaizoku/SaitamaRobot).
                  \n\nIf you have any question about rias, let us know at .""",
             parse_mode=ParseMode.MARKDOWN,
